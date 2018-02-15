7149dc5d3a60e09f66634a1d636ce7413420fb60
Nick Schneider <n.schneider@tum.de>
2018-02-15 13:38:22 +0100

    added doses routes!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:create, :index, :show, :new]
+  resources :cocktails, only: [:create, :index, :show, :new] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
 end
